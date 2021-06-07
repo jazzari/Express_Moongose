50e394bbcaed20ad32fb88bb184dbc51b7a0189f
jazzari <passionjazz@gmail.com>
2021-06-07 09:38:53 +0200

    Create show view to products

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { allProducts });
 
 })
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+})
+
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000");
 })
