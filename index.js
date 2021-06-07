8658976117a6d56306b94a4f51259deb880b7ad0
jazzari <passionjazz@gmail.com>
2021-06-07 11:02:47 +0200

    Add update action and edit view

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
     console.log('OH NOO!, MONGO CONNECTION ERROR!!!');
     console.log(err);
 })
+const methodOverride = require('method-override');
 const Product = require('./Models/product');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
     const allProducts = await Product.find({});
@@ -33,6 +36,16 @@ app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product });
+})
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product });
+})
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true});
+    res.redirect(`/products/${product._id}`);
 })
 
 app.listen(3000, () => {
